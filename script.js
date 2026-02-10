1ffa3198f2ced1dd7cbf87b51271d279cc54598b
Arnsgaming8 <arnavjug@gmail.com>
2026-02-09 19:25:42 -0700

    test_github_page

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("clickBtn").addEventListener("click", () => {
+    const output = document.getElementById("output");
+    output.textContent = "Button clicked! Your site is working.";
+});
